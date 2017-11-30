a84eed36ca01b3c5a0fd5ae5c7e7a82ebfe33136
RatiboR1978 <klov29@mail.ru>
2017-11-30 08:24:14 +0700

    Сверстал slider1, enim, slider2

--- js/common.js
+++ js/common.js
@@ -1,4 +1,59 @@
-$(function () {
+$(document).ready(function(){
+
+    $('.slider1__block').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
+
+    var arrImg = ['../img/slide2-2.jpg', '../img/slide2-1.jpg', '../img/slide2-3.jpg'],
+        j = 1,
+        k = 2,
+        i = 0;
+
+    $('.slider__prev').click(function () {
+        i--;
+        j--;
+        k--;
+        if (i < 0) {
+            i = arrImg.length - 1;
+        }
+        if (j < 0) {
+            j = arrImg.length - 1;
+        }
+        if (k < 0) {
+            k = arrImg.length - 1;
+        }
+        $(".slder2__image1 img").attr("src", arrImg[i]);
+        $(".slder2__image2 img").fadeTo(6, 0).attr("src", arrImg[j]).fadeTo(400, 1);
+        $(".slder2__image3 img").attr("src", arrImg[k]);
+    });
+
+    $('.slider__next').click(function () {
+        i++;
+        j++;
+        k++;
+        if (i > arrImg.length - 1) {
+            i = 0;
+        }
+        if (j > arrImg.length - 1) {
+            j = 0;
+        }
+        if (k > arrImg.length - 1) {
+            k = 0;
+        }
+        $(".slder2__image1 img").attr("src", arrImg[i]);
+        $(".slder2__image2 img").fadeTo(6, 0).attr("src", arrImg[j]).fadeTo(400, 1);
+        $(".slder2__image3 img").attr("src", arrImg[k]);
+    })
+
+
 
 
 
