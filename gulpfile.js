afe1c5c9af66ef15ca10b38d26800ddb211cf50b
RatiboR1978 <klov29@mail.ru>
2017-12-01 07:07:14 +0700

    Доверстал до конца добавил эффекты

--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,7 @@ gulp.task('scripts', function() {
 	return gulp.src([ // Берем все необходимые библиотеки
 		'libs/jquery/dist/jquery.min.js', // Берем jQuery
         'libs/slick-carousel/slick/slick.js', // Берем slick
+        'libs/wow/dist/wow.min.js', // Берем WoW
 		'libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
 		])
 		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
